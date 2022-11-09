43cc218e5523acd7fae15b0bfe9e99f37c23f558
helio977 <82115973+helio977@users.noreply.github.com>
2022-11-09 14:38:41 -0600

    agregue la partes de la rutas solo esta el login

--- imports/ui/components/App.jsx
+++ imports/ui/components/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Data } from './Data.jsx';
+import { Login } from './login.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
 	return (
-		<div>		
-			<Data />			
-		</div>
+		<BrowserRouter>
+			
+        <Routes>
+		<Route path="/"  element={ <Data /> } />
+		<Route path="/login" element={<Login />} />
+		</Routes>
+
+		
+		</BrowserRouter>
+
+		// <div>
+		// 	<Data></Data>
+		// </div>
+
 	);
 }
