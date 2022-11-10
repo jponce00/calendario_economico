f2dfabaecab58ac92916679d5ee641bdd77ef606
helio977 <82115973+helio977@users.noreply.github.com>
2022-11-09 22:05:47 -0600

    ya esta el login funcionando, usuario = admin y contraseña = admin

--- imports/ui/components/login.jsx
+++ imports/ui/components/login.jsx
@@ -9,25 +9,69 @@ export function Login() {
 
     const [user, setuser] = useState('');
     const [contrase, setcontrase] = useState('');
+    const [alerta, setalerta] = useState(false);
+    const navigate = useNavigate();
+
+     
+ 
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if(user == "admin" && contrase=="admin"){
+            setalerta(false);
+            navigate("/");
+
+        }
+        else{
+             
+            setalerta(true);
+        }
+        
+      }
+
+      const quitaralerta = () =>{
+        setalerta(false);
+      }
+
+
+
 
     return (
         
-        <div className='centrarlogin'>
+        <div className=''  >
 
-            <div className="">
-                <form className="login">
-                    <p className="title">PANEL DE ADMINISTRADOR</p>
-                    <input type="text" placeholder="Username" autoFocus required/>
-                    <i className="fa fa-user"><img className='' src="https://cdn-icons-png.flaticon.com/512/3661/3661442.png" alt="" height='17' width='17'></img> </i>
-                    <input type="password" placeholder="Password" required/>
-                    <i className="fa fa-key"> <img className='' src="https://cdn-icons-png.flaticon.com/512/483/483408.png" alt="" height='17' width='17'></img> </i>
-                    {/* <a href="#">¿Olvidaste tu contraseña?</a> */}
-                    <button>
-                    <i className="spinner"> </i>
-                    <span className="state">Log in</span>
-                    </button>
-                </form>
-            </div>
+
+                   
+                    
+                    <div className={alerta ? "show-element" : null}>
+                        {alerta && <div className="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong> Nombre de usuario o contraseña incorrectos </strong>
+                        <button type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={quitaralerta}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        </div>}
+                    </div>  
+
+
+                    <div className="centrarlogin">
+
+                    <form className="login" onSubmit={handleSubmit}>
+                        <p className="title">PANEL DE ADMINISTRADOR</p>
+                        <input type="text" placeholder="Username" autoFocus required  value={user}  onChange={(e) => setuser(e.target.value)}/>
+                        <i className="fa fa-user"><img className='' src="https://cdn-icons-png.flaticon.com/512/3661/3661442.png" alt="" height='17' width='17'></img> </i>
+                        <input type="password" placeholder="Password" required value={contrase}  onChange={(e) => setcontrase(e.target.value)} />
+                        <i className="fa fa-key"> <img className='' src="https://cdn-icons-png.flaticon.com/512/483/483408.png" alt="" height='17' width='17'></img> </i>
+                        {/* <a href="#">¿Olvidaste tu contraseña?</a> */}
+                        <button>
+                        <i className="spinner"> </i>
+                        <span className="state">Log in</span>
+                        </button>
+                    </form>
+                    </div>
+
+
+
 
         </div>
         
